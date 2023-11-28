3a9835dfe88d7eec00c3f67b6958316c2a476184
matheusdcunha <matheus.cunha.fonseca50@gmail.com>
2023-11-28 18:06:24 -0300

    Mudei a mensagem de erro, inseri para converter pressionanod ENTER e corrigi pequenos erros

--- js/modules/ConverterBinary.js
+++ js/modules/ConverterBinary.js
@@ -7,9 +7,15 @@ export default class ConverterBinary {
   }
 
   converterToDecimal(binary) {
+    const spanErro = document.createElement("span");
+    spanErro.innerText = "Insira um valor binário válido";
+    const parentBinary = this.binary.parentNode;
+
     if (!/^[01]+$/.test(binary)) {
-      return window.alert("Insira um número binário valido");
+      parentBinary.lastChild.innerHTML = "";
+      parentBinary.appendChild(spanErro);
     } else {
+      parentBinary.lastChild.innerHTML = "";
       if (!isNaN(binary)) binary = binary.toString();
       let decimal = 0;
       for (let i = binary.length - 1; i >= 0; i--) {
@@ -19,29 +25,33 @@ export default class ConverterBinary {
     }
   }
 
+  cleanBinaryNumber(binary) {
+    return binary.replace(/( )+/g, "");
+  }
+
   putValuesInput() {
-    const dec = this.converterToDecimal(this.binary.value);
-    this.decimalOutput.value = dec;
+    const binaryClean = this.cleanBinaryNumber(this.binary.value);
+    const dec = this.converterToDecimal(binaryClean);
+    if (dec !== undefined) {
+      this.binary.value = binaryClean;
+      this.decimalOutput.value = dec;
+    } else {
+      this.decimalOutput.value = "";
+    }
   }
 
   addEventButton() {
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        this.putValuesInput();
+      }
+    });
     this.button.addEventListener("click", this.putValuesInput);
   }
 
   init() {
     if (this.binary && this.decimalOutput && this.button) this.addEventButton();
+
     return this;
   }
 }
-
-// const binToDecimal = function (binary) {
-//   // Transformar o parametro em string, pois se for número o for não consegue acessar o método .length
-//   if (!isNaN(binary)) binary = binary.toString();
-//   let decimal = 0;
-
-//   for (let i = binary.length - 1; i >= 0; i--) {
-//     decimal += +binary[i] * Math.pow(2, binary.length - 1 - i);
-//   }
-
-//   return decimal;
-// };
